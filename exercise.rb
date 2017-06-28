6837e0341581049d27fa5e6ef144fb49b78368b6
maryfranks <maryfranks@gmail.com>
2017-06-28 13:53:43 -0400

    added arrays and hashes

---
+++ exercise.rb
@@ -0,0 +1,10 @@
+fav_colors = ["purple", "green", "grey", "black"]
+ages_of_fam = [34, 30, 33]
+coin_flip = ["heads", "heads", "tails", "heads", "tails"]
+performing_artists = ["soundgarden", "benny goodman", "bush"]
+fav_colors_symbols = [:purple, :green, :grey, :black]
+
+three_words = {array: "ordered, integer-indexed collections", hash: "collection of unique keys and their values", method: "named sequence of instructions"}
+movies = {"Jaws" => 1975, "Lake Placid" => 1999, "Jurassic Park" => 1993}
+cities = {toronto: 6418000, la: 3929000, cologne: 1017000}
+ages_of_fam_hash = {mary: 34, megan: 30, lindsey: 32}
