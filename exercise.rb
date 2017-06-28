325eb5e1e431b6dd2a2e7fceed25bde7bf298600
maryfranks <maryfranks@gmail.com>
2017-06-28 14:18:43 -0400

    exercise 2 more manipulation

--- exercise.rb
+++ exercise.rb
@@ -9,9 +9,20 @@ movies = {"Jaws" => 1975, "Lake Placid" => 1999, "Jurassic Park" => 1993}
 cities = {toronto: 6418000, la: 3929000, cologne: 1017000}
 ages_of_fam_hash = {mary: 34, megan: 30, lindsey: 32}
 
+# exercise 1
 puts coin_flip
 puts fav_colors[0]
 puts ages_of_fam.sort
 ages_of_fam << 0
 puts ages_of_fam
 puts movies["Jaws"]
+
+# exercise 2
+puts fav_colors[-1]
+cities[:boston] = 655000
+coin_flip.reverse!
+puts coin_flip
+puts cities[:la]
+performing_artists.each do |artist|
+  puts "I think #{artist} is great."
+end
